8db9ba318310563af05bece964b3cde06b236cce
Shane McDonough <shanemcdman@gmail.com>
2025-12-11 16:08:04 -0800

    reorder keymaps a bit

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,10 +4,12 @@
 
 local map = LazyVim.safe_keymap_set
 
-map("n", "<leader>r", vim.lsp.buf.rename)
+map("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename Symbol" })
 -- not using map to overwrite existing mapping
 vim.keymap.set("", "<leader><space>", "gc", { remap = true })
 vim.keymap.set("n", "<leader>g=", "<leader>ca", { remap = true })
+
+-- delete keymaps included in lazyvim I don't like
 vim.keymap.del("n", "<leader>qq") -- I don't know where this comes from and I don't want it
 vim.keymap.del("n", "<leader>wm") -- "Something with snacks" Whatever, I don't want it
 vim.keymap.del("n", "<leader>wd") -- "Delete window" I don't want it
