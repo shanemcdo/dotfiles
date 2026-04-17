61be2e9ed845da535c332b1197715d582deaff81
Shane Walker <shanemcdman@gmail.com>
2026-04-17 13:40:57 -0700

    remove remapping of leader r

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = LazyVim.safe_keymap_set
-
-map("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename Symbol" })
 -- not using map to overwrite existing mapping
 vim.keymap.set("", "<leader><space>", "gc", { remap = true })
 vim.keymap.set("n", "<leader>g=", "<leader>ca", { remap = true })
